e6ac59c3528d9b555b62e4970a53e3b3af6aa7ad
Hentsuna <damtrongj095@gmail.com>
2024-03-25 21:31:39 +0700

    update
    Tạo radiogroup để nhóm 3 nút radio lại với nhau

--- Cau1_AppVeHinh/src/ntu_63135896/ManHinhChinh.java
+++ Cau1_AppVeHinh/src/ntu_63135896/ManHinhChinh.java
@@ -2,14 +2,17 @@ package ntu_63135896;
 
 import java.awt.EventQueue;
 
+import javax.swing.ButtonGroup;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 import javax.swing.border.EmptyBorder;
 
 public class ManHinhChinh extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private final ButtonGroup b = new ButtonGroup();
 	
 
 	/**
@@ -22,6 +25,21 @@ public class ManHinhChinh extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		
+		JRadioButton nutA = new JRadioButton("Hình vuông");
+		b.add(nutA);
+		nutA.setBounds(71, 0, 96, 43);
+		contentPane.add(nutA);
+		
+		JRadioButton nutB = new JRadioButton("Hình tròn");
+		b.add(nutB);
+		nutB.setBounds(169, 0, 96, 43);
+		contentPane.add(nutB);
+		
+		JRadioButton nutC = new JRadioButton("Hình elip");
+		b.add(nutC);
+		nutC.setBounds(267, 5, 86, 33);
+		contentPane.add(nutC);
 	}
 
 }
