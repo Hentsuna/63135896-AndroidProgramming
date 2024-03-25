03be97eece41f619ef2b9b73fd5bed6655ff8416
Hentsuna <damtrongj095@gmail.com>
2024-03-25 21:27:19 +0700

    update
    Chạy màn hình chính ở chương trình chính

--- Cau1_AppVeHinh/src/ntu_63135896/ChuongTrinhChinh.java
+++ Cau1_AppVeHinh/src/ntu_63135896/ChuongTrinhChinh.java
@@ -3,7 +3,7 @@ package ntu_63135896;
 public class ChuongTrinhChinh {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		new ManHinhChinh().setVisible(true);
 
 	}
 
--- Cau1_AppVeHinh/src/ntu_63135896/ManHinhChinh.java
+++ Cau1_AppVeHinh/src/ntu_63135896/ManHinhChinh.java
@@ -10,22 +10,7 @@ public class ManHinhChinh extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					ManHinhChinh frame = new ManHinhChinh();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+	
 
 	/**
 	 * Create the frame.
