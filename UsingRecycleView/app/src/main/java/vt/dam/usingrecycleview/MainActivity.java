196cf807e180b478003f764a2e2cf4a61e9a668a
Hentsuna <damtrongj095@gmail.com>
2024-04-14 21:58:19 +0700

    update

--- UsingRecycleView/app/src/main/java/vt/dam/usingrecycleview/MainActivity.java
+++ UsingRecycleView/app/src/main/java/vt/dam/usingrecycleview/MainActivity.java
@@ -7,12 +7,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     LandScapeAdapter landScapeAdapter;
-    ArrayList<LandScape> recyclerViewData;
+    ArrayList<LandScape> recyclerViewDatas;
+    RecyclerView recyclerViewLandscape;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,5 +26,26 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        //3
+        recyclerViewDatas = getDataForRecyclerView();
+        //4
+        recyclerViewLandscape = findViewById(R.id.recycleLand);
+        //5
+        RecyclerView.LayoutManager layoutLinear = new LinearLayoutManager(this);
+        recyclerViewLandscape.setLayoutManager(layoutLinear);
+        //6
+        landScapeAdapter = new LandScapeAdapter(this, recyclerViewDatas);
+        //7
+        recyclerViewLandscape.setAdapter(landScapeAdapter);
+    }
+
+    ArrayList<LandScape> getDataForRecyclerView(){
+        ArrayList<LandScape> dsDuLieu = new ArrayList<LandScape>();
+        LandScape landScape1 = new LandScape("Screenshot (53)", "Chiêu thức lửa");
+        dsDuLieu.add(landScape1);
+        dsDuLieu.add(new LandScape("Screenshot (56)", "Chiêu thức điện"));
+        dsDuLieu.add(new LandScape("Screenshot (61)", "Chiêu thức nước"));
+        dsDuLieu.add(new LandScape("Screenshot (63)", "Chiêu thức cỏ"));
+        return dsDuLieu;
     }
 }
