29aa08921f91b5e9624915184f2108edcc9e08ed
Hentsuna <damtrongj095@gmail.com>
2024-04-29 23:29:05 +0700

    update

--- TH_IntentLogin_VD2/app/src/main/java/vt/dam/th_intentlogin_vd2/ActivityLogin.java
+++ TH_IntentLogin_VD2/app/src/main/java/vt/dam/th_intentlogin_vd2/ActivityLogin.java
@@ -1,6 +1,11 @@
 package vt.dam.th_intentlogin_vd2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +14,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class ActivityLogin extends AppCompatActivity {
+    EditText ten, matKhau, email;
+    Button nutXacNhan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,32 @@ public class ActivityLogin extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        TimDieuKhien();
+        nutXacNhan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ChuyenManHinhHome();
+            }
+        });
+    }
+
+    void TimDieuKhien(){
+        ten = findViewById(R.id.edtUserName);
+        matKhau = findViewById(R.id.edtPass);
+        email = findViewById(R.id.edtMail);
+        nutXacNhan = findViewById(R.id.btnOK);
+    }
+
+    void ChuyenManHinhHome(){
+        String tenDN = ten.getText().toString();
+        String matKhauDN = matKhau.getText().toString();
+        String emailDN = email.getText().toString();
+        if(tenDN == "63CNTT2" || matKhauDN == "123456"){
+            Intent iHome = new Intent(this, ActivityHome.class);
+            startActivity(iHome);
+        }else {
+            Toast toast = Toast.makeText(this, "Sai thông tin đăng nhập", Toast.LENGTH_LONG);
+            toast.show();
+        }
     }
 }
