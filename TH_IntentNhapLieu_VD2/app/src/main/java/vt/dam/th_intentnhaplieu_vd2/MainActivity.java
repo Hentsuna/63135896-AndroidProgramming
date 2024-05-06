0e0f3f0ad9ede1b2c0f9569ba4206ad4828c0510
Hentsuna <damtrongj095@gmail.com>
2024-05-06 15:12:53 +0700

    Update MainActivity.java

--- TH_IntentNhapLieu_VD2/app/src/main/java/vt/dam/th_intentnhaplieu_vd2/MainActivity.java
+++ TH_IntentNhapLieu_VD2/app/src/main/java/vt/dam/th_intentnhaplieu_vd2/MainActivity.java
@@ -1,6 +1,10 @@
 package vt.dam.th_intentnhaplieu_vd2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    Button nhapLieu;
+    TextView ten, nam;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,23 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        LayDieuKhien();
+        nhapLieu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NhapLieu();
+            }
+        });
+    }
+
+    void LayDieuKhien(){
+        nhapLieu = findViewById(R.id.btnChuyenMH);
+        ten = findViewById(R.id.edtTenNhan);
+        nam = findViewById(R.id.edtNamNhan);
+    }
+
+    public void NhapLieu(){
+        Intent iNhap = new Intent(this, NhapLieuActivity.class);
+        startActivityForResult(iNhap, 8000);
     }
 }
