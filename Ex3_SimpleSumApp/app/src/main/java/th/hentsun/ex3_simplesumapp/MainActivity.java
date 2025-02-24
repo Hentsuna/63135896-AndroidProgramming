735e3f7cb80a1ec917ddbe93090de086aee7d82d
DamVietTrong <trong.dv@dktech.info>
2025-02-24 16:36:41 +0700

    Update ex3

--- Ex3_SimpleSumApp/app/src/main/java/th/hentsun/ex3_simplesumapp/MainActivity.java
+++ Ex3_SimpleSumApp/app/src/main/java/th/hentsun/ex3_simplesumapp/MainActivity.java
@@ -1,6 +1,10 @@
 package th.hentsun.ex3_simplesumapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText edt1, edt2, edt3;
+    Button btn1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +26,19 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        edt1 = findViewById(R.id.edt1);
+        edt2 = findViewById(R.id.edt2);
+        edt3 = findViewById(R.id.edt3);
+        btn1 = findViewById(R.id.button);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int tong = (Integer) Integer.parseInt(String.valueOf(edt1.getText())) + Integer.parseInt(String.valueOf(edt2.getText()));
+
+                edt3.setText(String.valueOf(tong));
+            }
         });
     }
 }
