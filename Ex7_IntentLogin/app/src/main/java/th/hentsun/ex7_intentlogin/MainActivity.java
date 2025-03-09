e15a832bb08be3cf81a13b098dcc58e3d3754d2e
DamVietTrong <trong.dv@dktech.info>
2025-03-10 00:43:29 +0700

    update ex7

--- Ex7_IntentLogin/app/src/main/java/th/hentsun/ex7_intentlogin/MainActivity.java
+++ Ex7_IntentLogin/app/src/main/java/th/hentsun/ex7_intentlogin/MainActivity.java
@@ -1,6 +1,9 @@
 package th.hentsun.ex7_intentlogin;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    Button nutLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +24,17 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        nutLogin = findViewById(R.id.btnLogin);
+        nutLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ChuyenManHinhLogin();
+            }
+        });
+    }
+
+    public void ChuyenManHinhLogin(){
+        Intent iLogin = new Intent(this, ActivityLogin.class);
+        startActivity(iLogin);
     }
 }
