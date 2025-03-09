7ced09d8d831b5b82641633bd3999e72c6155d88
DamVietTrong <trong.dv@dktech.info>
2025-03-09 23:54:19 +0700

    Update MainActivity.java

--- Ex6_IntentDonGian/app/src/main/java/th/hentsun/ex6_intentdongian/MainActivity.java
+++ Ex6_IntentDonGian/app/src/main/java/th/hentsun/ex6_intentdongian/MainActivity.java
@@ -1,6 +1,9 @@
 package th.hentsun.ex6_intentdongian;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    Button sw, ff;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +23,25 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        sw = findViewById(R.id.button);
+        ff = findViewById(R.id.button2);
+
+        sw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentsw = new Intent(MainActivity.this, SWActivity.class);
+                startActivity(intentsw);
+            }
+        });
+
+        ff.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentff = new Intent(MainActivity.this, FFActivity2.class);
+                startActivity(intentff);
+            }
         });
     }
 }
