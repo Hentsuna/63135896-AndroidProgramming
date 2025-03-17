77e63044aafcc8a92ec1e3bce904286d47ef94fe
DamVietTrong <trong.dv@dktech.info>
2025-03-17 13:10:40 +0700

    update

--- Modern_Material_Dashboard/app/src/main/java/th/hentsun/modern_material_dashboard/MainActivity.java
+++ Modern_Material_Dashboard/app/src/main/java/th/hentsun/modern_material_dashboard/MainActivity.java
@@ -1,20 +1,30 @@
 package th.hentsun.modern_material_dashboard;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    CardView clothingCard;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-
+        clothingCard = findViewById(R.id.clothingCard);
+        clothingCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ClothingActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
