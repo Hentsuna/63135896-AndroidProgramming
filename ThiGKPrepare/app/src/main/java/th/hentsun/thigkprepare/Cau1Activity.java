d8d370592146a380397ee9acd748f25415dee012
DamVietTrong <trong.dv@dktech.info>
2025-03-30 22:30:59 +0700

    update cau1

--- ThiGKPrepare/app/src/main/java/th/hentsun/thigkprepare/Cau1Activity.java
+++ ThiGKPrepare/app/src/main/java/th/hentsun/thigkprepare/Cau1Activity.java
@@ -1,6 +1,9 @@
 package th.hentsun.thigkprepare;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +12,66 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Cau1Activity extends AppCompatActivity {
+    EditText txt1, txt2, txt3;
+    Button btn1, btn2, btn3, btn4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_cau1);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        txt1 = findViewById(R.id.edt1);
+        txt2 = findViewById(R.id.edt2);
+        txt3 = findViewById(R.id.edt3);
+        btn1 = findViewById(R.id.button1);
+        btn2 = findViewById(R.id.button2);
+        btn3 = findViewById(R.id.button3);
+        btn4 = findViewById(R.id.button4);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Cong();
+            }
         });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Tru();
+            }
+        });
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Nhan();
+            }
+        });
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Chia();
+            }
+        });
+
+    }
+
+    void Cong(){
+        int kq = Integer.parseInt(txt1.getText().toString()) + Integer.parseInt(txt2.getText().toString());
+        txt3.setText(String.valueOf(kq));
+    }
+    void Tru(){
+        int kq = Integer.parseInt(txt1.getText().toString()) - Integer.parseInt(txt2.getText().toString());
+        txt3.setText(String.valueOf(kq));
+    }
+    void Nhan(){
+        int kq = Integer.parseInt(txt1.getText().toString()) * Integer.parseInt(txt2.getText().toString());
+        txt3.setText(String.valueOf(kq));
+    }
+    void Chia(){
+        if (Integer.parseInt(txt2.getText().toString()) == 0){
+            txt3.setText("Không thể chia cho 0");
+        }else {
+            double kq = Double.parseDouble(txt1.getText().toString()) / Double.parseDouble(txt2.getText().toString());
+            txt3.setText(String.valueOf(kq));
+        }
     }
 }
--- ThiGKPrepare/app/src/main/java/th/hentsun/thigkprepare/MainActivity.java
+++ ThiGKPrepare/app/src/main/java/th/hentsun/thigkprepare/MainActivity.java
@@ -1,6 +1,9 @@
 package th.hentsun.thigkprepare;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +12,45 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    Button nut1, nut2, nut3, nut4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        nut1 = findViewById(R.id.button1);
+        nut2 = findViewById(R.id.button2);
+        nut3 = findViewById(R.id.button3);
+        nut4 = findViewById(R.id.button4);
+
+        nut1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(MainActivity.this, Cau1Activity.class);
+                startActivity(intent1);
+            }
+        });
+        nut2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(MainActivity.this, Cau2Activity.class);
+                startActivity(intent1);
+            }
+        });
+        nut3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(MainActivity.this, Cau3Activity.class);
+                startActivity(intent1);
+            }
+        });
+        nut4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(MainActivity.this, Cau4Activity.class);
+                startActivity(intent1);
+            }
         });
     }
 }
