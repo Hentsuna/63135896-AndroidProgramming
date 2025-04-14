5f5b88d70e149f7e8151e835e9b96929e91a0f3d
Hentsuna <damtrongj095@gmail.com>
2025-04-15 00:55:26 +0700

    update

--- ReplaceFragmentDynamic/app/src/main/java/th/hentsun/replacefragmentdynamic/MainActivity.java
+++ ReplaceFragmentDynamic/app/src/main/java/th/hentsun/replacefragmentdynamic/MainActivity.java
@@ -1,4 +1,4 @@
-package th.hentsun.fragmentdynamic;
+package th.hentsun.replacefragmentdynamic;
 
 import android.os.Bundle;
 
@@ -7,8 +7,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,11 +15,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        fragmentManager.beginTransaction()
-                .add(R.id.fragmentContainerView, new ContentFragment())
-                .add(R.id.fragmentContainerView2, new Fragment())
-                .commit();
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
     }
 }
